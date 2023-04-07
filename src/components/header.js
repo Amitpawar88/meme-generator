65359463e6415ef90e7ab3924f3f29c3d8383a59
Amit Pawar <amitpawar7475@gmail.com>
2023-04-07 23:37:24 +0530

    final commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="../images/troll-face.png" />
+            <h1 className="header--title">Meme Generator</h1>
+            <h3 className="header--project">- Amit</h3>
+        </header>
+    )
+}
